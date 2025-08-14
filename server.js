bd3f501b5730abd0bbf452e442563b172e201109
Cristian Silvera <cristiansilveraa@gmail.com>
2025-08-13 23:43:46 -0300

    doc: start courses API

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
-const http = require('node:http')
+const fastify = require ('fastify')
+const crypto = require('crypto')
 
-const server = http.createServer((request, replay) => {
-    replay.write("Hello World")
+const server = fastify()
 
-    replay.end()
-} )
+const courses = [
+    {id: "1", title: 'Curso de Node.js'},
+    {id: "2", title: 'Curso de React'},
+    {id: "3", title: 'Curso de React Native'}
+]
 
-server.listen(3333).on('listening', () => {
-    console.log('HTTP server running!')
-});
+server.get('/courses', () => {
+    return { courses }
+})
 
+server.get('/courses/:id', (request, replay) => {
+    const courseId = request.params.id
+    
+    const course = courses.find(course => course.id === courseId)
+
+    if (course) {
+        return { course }
+    }
+
+    return replay.status(404).send()
+
+})
+
+server.post('/courses', (request, reply) => {
+
+    const courseId = crypto.randomUUID()
+
+    courses.push({id: courseId, title: 'Nuevo curso'})
+
+    return reply.status(201).send({courseId})
+})
+
+server.listen({ port:3333 }).then(() => {
+    console.log("HTTP server running!!!")
+})
