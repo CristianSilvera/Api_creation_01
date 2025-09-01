dd1c5dc9be09692405d41bb3d40a16929112ce28
Cristian Silvera <cristiansilveraa@gmail.com>
2025-09-01 12:39:46 -0300

    feat: add filtering, ordering

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,7 +1,9 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import { db } from "../database/client.ts"
 import { courses } from "../database/schema.ts"
+import { ilike, asc } from 'drizzle-orm'
 import z from 'zod'
+
 
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -9,6 +11,10 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         schema: {
             tags: ['courses'],
             summary: 'Get all courses',
+            querystring: z.object({
+                search: z.string().optional(),
+                orderBy: z.enum(['id', 'title']).optional().default('id'),
+            }),
             response: {
                 200: z.object({
                     courses: z.array(z.object({
@@ -22,15 +28,21 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         }
 
     }, async (request, reply) => {
-    
-        const result = await db.select({
-    
-            id: courses.id,
-            title: courses.title,
-    
-    
-            }).from(courses)
-    
+
+        const { search, orderBy } = request.query
+
+        const result = await db
+            .select({
+                id: courses.id,
+                title: courses.title,
+            })
+            .from(courses)
+            .orderBy (asc(courses[orderBy]))
+            .where (
+                
+                search ? ilike(courses.title, `%${search}%`) : undefined
+            )
+
         return reply.send({ courses: result })
     })
 }
